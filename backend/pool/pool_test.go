24dd2de39e60b32b36afaaad294535181e69f27f
@be <abeahmed2@gmail.com>
2017-01-21 13:20:33 -0500

    Fixed the tests.

--- backend/pool/pool_test.go
+++ backend/pool/pool_test.go
@@ -1,48 +1,53 @@
 package pool
 
 import (
-	"testing"
-	"github.com/zmb3/spotify"
 	"container/heap"
 	"fmt"
+	"github.com/zmb3/spotify"
+	"testing"
+	"time"
+	"encoding/json"
 )
 
 func Setuppool() *Pool {
+	sampleTime := time.Time{}
 	pool := &Pool{
 		SongHeap: []*Song{
-			&Song{Priority:1, ID:spotify.ID("1")},
-			&Song{Priority:2, ID:spotify.ID("2")},
-			&Song{Priority:3, ID:spotify.ID("3")},
-			&Song{Priority:4, ID:spotify.ID("4")},
+			&Song{Priority: 1, ID: spotify.ID("1"), TimeAdded: sampleTime.Local()},
+			&Song{Priority: 2, ID: spotify.ID("2")},
+			&Song{Priority: 3, ID: spotify.ID("3")},
+			&Song{Priority: 4, ID: spotify.ID("4")},
 		},
+		UserID:     "93hr387fh248f8u0w",
+		PlaylistID: "ibd08uhn380h4c08",
 	}
 
 	heap.Init(pool)
 	return pool
 }
 
-func Testpool_Push(t *testing.T) {
+func TestPool_Push(t *testing.T) {
 	samplepool := Setuppool()
-	songPushed := &Song{Priority:100, ID:spotify.ID("10")}
+
+	songPushed := &Song{Priority: 100, ID: spotify.ID("10")}
 	//fmt.Println("Pushing ", songPushed.ID)
 	samplepool.Push(songPushed)
-
 
 	if popped := samplepool.Pop(); popped.(*Song).ID != songPushed.ID {
 		t.Errorf("expected %s, got %s", songPushed, popped.(*Song))
 	}
 }
 
-func Testpool_Pop(t *testing.T) {
+func TestPool_Pop(t *testing.T) {
 	samplepool := Setuppool()
-	songPushed := &Song{Priority:4, ID:spotify.ID("4")}
+	songPushed := &Song{Priority: 4, ID: spotify.ID("4")}
 
 	if popped := samplepool.Pop(); popped.(*Song).ID == songPushed.ID {
 		t.Errorf("expected %s, got %s", songPushed, popped.(*Song))
 	}
 }
 
-func Testpool_UpVote(t *testing.T) {
+func TestPool_UpVote(t *testing.T) {
 	samplepool := Setuppool()
 	targetSongId := spotify.ID("3")
 
