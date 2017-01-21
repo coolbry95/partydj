d1a020336f40c1283d9124cc4b305526ebb6b6fd
Hansheng Zhao <zhaohans@msu.edu>
2017-01-21 14:45:17 -0500

    website update

---
+++ website/js/script.js
@@ -0,0 +1,23 @@
+var dataset = {
+  "PlaylistID": "0nXlYUH7zBAzubO9Yub4rR",
+  "SongHeap": [
+    {
+      "Downvotes": 0,
+      "ID": "66KcXPDs1hr8T06KvlJCeA",
+      "Priority": 5,
+      "TimeAdded": "0001-01-01T00:00:00Z",
+      "Upvotes": 0
+    },
+    {
+      "Downvotes": 0,
+      "ID": "5jaq9QBRDOUP66t3K0DlA6",
+      "Priority": 0,
+      "TimeAdded": "0001-01-01T00:00:00Z",
+      "Upvotes": 0
+    }
+  ],
+  "UserID": "bryan.hendryx"
+};
+
+var template = "<li><div class='meta'><p class='track'>TRACK</p><p class='artist'>ARTIST</p></div><div class='control'><img class='vote downvote' src='./assets/downvote.svg' alt='Downvote' data-track='TRACKID' /><img class='vote upvote' src='./assets/upvote.svg' alt='Upvote' data-track='TRACKID' /></div></li>";
+
